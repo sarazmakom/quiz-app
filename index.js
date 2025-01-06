5ec0d1556065dd6e6ac7625abc8e41b78f758e2c
Sara Zmak <zmaksara@gmail.com>
2025-01-06 17:07:53 +0100

    add html and js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // phew… not a lot going on here. Please add some code!
+const card = document.querySelector("[data-js='card']");
+const answerButton = card.querySelector("[data-js='answer-button']");
+const answerDisplay = card.querySelector("[data-js='answer-display']");
+// const bookmarkButton = card.querySelector("[data-js='bookmark-button'");
+
+answerButton.addEventListener("click", () => {
+  const isAnswerVisible = answerDisplay.classList.contains(
+    "card__answer--active"
+  );
+
+  if (isAnswerVisible) {
+    answerDisplay.classList.remove("card__answer--active");
+    answerButton.textContent = "Show answer";
+  } else {
+    answerDisplay.classList.add("card__answer--active");
+    answerButton.textContent = "Hide answer";
+  }
+});
