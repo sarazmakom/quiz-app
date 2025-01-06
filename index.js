4b8226e7d9b3d4a880c4ec700031b589d6bbf0d6
Sara Zmak <zmaksara@gmail.com>
2025-01-06 17:31:11 +0100

    add defer to script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const card = document.querySelector("[data-js='card']");
 const answerButton = card.querySelector("[data-js='answer-button']");
 const answerDisplay = card.querySelector("[data-js='answer-display']");
 // const bookmarkButton = card.querySelector("[data-js='bookmark-button'");
-
+// console.log("here", answerButton);
 answerButton.addEventListener("click", () => {
   const isAnswerVisible = answerDisplay.classList.contains(
     "card__answer--active"
